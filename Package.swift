f0d5d1cf58f09c0828d0fa103b493a14d2dee254
Luca Peterschilka <luca.peterschilka@gmail.com>
2021-04-11 21:11:36 +0200

    Refactored main to use ArgumentParser by apple

--- Package.swift
+++ Package.swift
@@ -16,17 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "qbio",
-            dependencies: ["qbio-lib"]),
-        .testTarget(
-            name: "qbioTests",
-            dependencies: ["qbio"]),
+            dependencies: ["qbio-lib", .product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .target(
             name: "qbio-lib",
             dependencies: []),
--- Sources/qbio/main.swift
+++ Sources/qbio/main.swift
@@ -1,42 +1,7 @@
 import qbio_lib
 
-let artistDataSource = AudioDBArtistDataSource()
+// Dependency must be passed to 
+QBio.artistDataSource = AudioDBArtistDataSource()
 
-executeArguments()
-
-func executeArguments() {
-    do {
-        for arg in ArgumentParser().parsedArguments {
-            switch arg {
-            case .help:
-                printUsage()
-            case .artistValue:
-                // Value is guaranteed for .artistValue and can safely accessed
-                try printBio(artist: arg.value!)
-            default:
-                continue
-            }
-        }
-    } catch {
-        print("An error occurred while executing the query. Please try again later.")
-    }
-}
-
-func printBio(artist: String) throws {
-    let bio = try artistDataSource.getBio(artist: artist)
-    print(artist)
-    print("--------------------")
-    print(bio)
-
-    // print an empty line for better visual separation for multiple artists
-    print()
-}
-
-func printUsage() {
-    print("USAGE")
-    print(
-        "\tqbio <artist(s) to query>  -- Values with whitespace needs to be surrounded by quotes (\"). Multiple artists can be queried by separating with whitespace"
-    )
-    print("OPTIONS")
-    print("\t--help -h\tPrints this manual")
-}
+// Run the application
+QBio.main()
